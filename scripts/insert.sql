fbafaeb85781e3f781b245b1bd839ab338f90ee2
Iri <110794234+Irinek0@users.noreply.github.com>
2025-04-19 21:37:54 -0400

    Add example insert statements

---
+++ scripts/insert.sql
@@ -0,0 +1,17 @@
+-- Tuple insertion examples
+
+-- Book entity
+INSERT INTO BOOK
+VALUES (1463027589, '2nd', 'Ranger Guide to Shoshone National Forest', 27, 1989, 'Outdoors Education', 13.99, 'English', 56, 'Wyoming Press');
+
+-- Publisher entity
+INSERT INTO PUBLISHER
+VALUES (74, 'Cheyenne', '350 W 24th St', 'WY', 'Wyoming Press', 'contactwp@wyopress.com', '+1-307-233-1746');
+
+-- Author entity
+INSERT INTO Author
+VALUES (163, 'Delilah A. June', 08-23-1946, 'American')
+
+-- Customer entity
+INSERT INTO Customer
+VALUES (1330, 'Henry', 'Jules', '1537 Pine St. Boulder, Colorado, 80302', '1537 Pine St. Boulder, Colorado, 80302', 7591357169425158,  02-31-1990, 757, 'Henry', 'Jules', 'henryj89tfl@gmail.com', 3033062414)
